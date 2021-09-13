fbac9911b660cee9ea5adad3a2df129755e60c62
Bruth98 <77505812+Bruth98@users.noreply.github.com>
2021-09-13 12:32:05 -0400

    Update SportsCar.java

--- SportsCar.java
+++ SportsCar.java
@@ -6,7 +6,7 @@ public class SportsCar extends Vehicle{  // Sports Car is a type of Vehicle
         this.description = "Sports Car";  // Set the description
     }
 
-    public double getCost() {
+    public double getCost() {  // Implements the getCost method from the Vehicle Class
         return 30000;  // Set the cost
     }
 }
