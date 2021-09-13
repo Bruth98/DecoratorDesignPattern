efce29a787dcedc9516e23914d6fa38f48a75660
Bruth98 <77505812+Bruth98@users.noreply.github.com>
2021-09-13 12:31:17 -0400

    Update Sedan.java

--- Sedan.java
+++ Sedan.java
@@ -6,7 +6,7 @@ public class Sedan extends Vehicle{  // Sedan is a type of Vehicle
         this.description = "Sedan";  // Set the description of a Sedan
     }
 
-    public double getCost() {
+    public double getCost() {  // implements the getCost method from the Vehicle Class
         return 20000;  // Set the price of a Sedan
     }
 }
