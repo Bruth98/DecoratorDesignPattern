91c76f750bb51c6de2bff5bcff01eb5ce1b3326e
Bruth98 <77505812+Bruth98@users.noreply.github.com>
2021-09-13 12:18:48 -0400

    Update Rims.java

--- Rims.java
+++ Rims.java
@@ -2,18 +2,18 @@
 Written by Brady Ruth
 */
 public class Rims extends VehicleDecorator{
-    Vehicle vehicle;
+    Vehicle vehicle;  // Establish a new Vehicle
 
     public Rims(Vehicle vehicle) {
-        this.vehicle = vehicle;
+        this.vehicle = vehicle;  // Create an instance of a new Vehicle
     }
 
     public String getDescription() {
-        return vehicle.getDescription() + ", cool rims";
+        return vehicle.getDescription() + ", cool rims";  // Adds a description of the Rims to the Vehicle
     }
 
     public double getCost() {
-        return vehicle.getCost() + 200;
+        return vehicle.getCost() + 200;  // Adds the price of the Rims to the Vehicle
     }
     
 }
