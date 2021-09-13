bafb4f4c00630300cd27f2a106b4c0736020a8f6
Bruth98 <77505812+Bruth98@users.noreply.github.com>
2021-09-13 12:25:31 -0400

    Update Sedan.java

--- Sedan.java
+++ Sedan.java
@@ -1,12 +1,12 @@
 /*
 Written by Brady Ruth
 */
-public class Sedan extends Vehicle{
+public class Sedan extends Vehicle{  // Sedan is a type of Vehicle
     public Sedan() {
-        this.description = "Sedan";
+        this.description = "Sedan";  // Set the description of a Sedan
     }
 
     public double getCost() {
-        return 20000;
+        return 20000;  // Set the price of a Sedan
     }
 }
