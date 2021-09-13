9398eb883f4338c56c90f8f60b8a527dd296d7de
Bruth98 <77505812+Bruth98@users.noreply.github.com>
2021-09-13 12:26:46 -0400

    Update SoundSystem.java

--- SoundSystem.java
+++ SoundSystem.java
@@ -1,19 +1,19 @@
 /*
 Written by Brady Ruth
 */
-public class SoundSystem extends VehicleDecorator{
-    Vehicle vehicle;
+public class SoundSystem extends VehicleDecorator{  // SoundSystem is a Vehicle modifier
+    Vehicle vehicle;  // Establish a Vehicle
 
-    public SoundSystem(Vehicle vehicle) {
+    public SoundSystem(Vehicle vehicle) {  // Create an instance of a new Vehicle
         this.vehicle = vehicle;
     }
 
     public String getDescription() {
-        return vehicle.getDescription() + ", awesome sound";
+        return vehicle.getDescription() + ", awesome sound";  // Set the new description
     }
 
     public double getCost() {
-        return vehicle.getCost() + 350;
+        return vehicle.getCost() + 350;  // Adds the price of a Sound System to the total cost
     }
     
 }
