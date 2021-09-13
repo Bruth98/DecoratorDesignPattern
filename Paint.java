bc86af6384335974d3d3b4228d77f5f7e4e657c8
Bruth98 <77505812+Bruth98@users.noreply.github.com>
2021-09-13 12:24:34 -0400

    Update Paint.java

--- Paint.java
+++ Paint.java
@@ -1,19 +1,19 @@
 /*
 Written by Brady Ruth
 */
-public class Paint extends VehicleDecorator{
-    Vehicle vehicle;
+public class Paint extends VehicleDecorator{  // This will be a modifier applied to a Vehicle
+    Vehicle vehicle;  // Establish a Vehicle
 
-    public Paint(Vehicle vehicle) {
+    public Paint(Vehicle vehicle) {  // Create an instance of a new Vehicle
         this.vehicle = vehicle;
     }
 
     public String getDescription() {
-        return vehicle.getDescription() + ", fancy paint";
+        return vehicle.getDescription() + ", fancy paint";  // Update the description of the Vehicle
     }
 
     public double getCost() {
-        return vehicle.getCost() + 150;
+        return vehicle.getCost() + 150;  // Adds the price of paint to the total cost
     }
     
 }
