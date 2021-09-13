0942728a3eaa818d8434b607113b833c30c26e05
Bruth98 <77505812+Bruth98@users.noreply.github.com>
2021-09-13 12:29:10 -0400

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -2,11 +2,11 @@
 Written by Brady Ruth
 */
 public abstract class Vehicle {
-    protected String description;
+    protected String description;  // Each Vehicle will have a String to describe its type, and any modifiers applied to it
 
     public String getDescription() {
-        return description;
+        return description;  // returns the description of the Vehicle
     }
 
-    public abstract double getCost();
+    public abstract double getCost();  // will return the total cost of the Vehicle. Still needs to be implemented elsewhere
 }
